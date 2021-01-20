fc8fd1e16f215c347987b453a08003a3985d76f8
KhoaMoya <42571291+KhoaMoya@users.noreply.github.com>
2021-01-20 23:05:05 +0700

    add button

--- KhoaPV1/Assignment_01_Git_Base/app/src/main/java/com/khoapv1/assignment_01_git_base/MainActivity.kt
+++ KhoaPV1/Assignment_01_Git_Base/app/src/main/java/com/khoapv1/assignment_01_git_base/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.khoapv1.assignment_01_git_base
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnToast.setOnClickListener {
+            Toast.makeText(this, "xin chào", Toast.LENGTH_LONG).show()
+        }
     }
 }
